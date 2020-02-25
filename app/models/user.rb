fb191c1cf12c1e5de015cdc823ec2c293ec7cbb6
maxime garcia <maxime-garcia@windowslive.com>
2020-02-25 15:55:02 +0100

    cloudinary ok

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_one_attached :photo
+
   has_many :forecasts
   has_many :user_leagues
   has_many :leagues, through: :user_leagues
