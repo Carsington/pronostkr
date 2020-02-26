f954383087c8883d5adfb989eb65557a33e43ef1
maxime garcia <maxime-garcia@windowslive.com>
2020-02-26 15:40:57 +0100

    changes ok

--- app/controllers/leagues_controller.rb
+++ app/controllers/leagues_controller.rb
@@ -1,10 +1,10 @@
 class LeaguesController < ApplicationController
 
-<<<<<<< HEAD
+
   def show
     @league = League.find(params[:id])
   end
-=======
+
  def create
   @league = League.new(league_params)
   user_league = UserLeague.new(user: current_user, league: @league, is_owner: true)
@@ -25,6 +25,6 @@ private
 def league_params
   params.require(:league).permit(:name, :competition_id)
 end
->>>>>>> a22f5a464c095084c86c2f80b943be49cc895175
+
 
 end
