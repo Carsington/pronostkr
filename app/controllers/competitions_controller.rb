77345409d36156b2c7679708753dd082d24052ed
Pierre-Loïc Pichon <pierreloic.pichon@gmail.com>
2020-02-26 18:03:43 +0100

    remove authentication to access competition page

--- app/controllers/competitions_controller.rb
+++ app/controllers/competitions_controller.rb
@@ -1,4 +1,6 @@
 class CompetitionsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:show]
+
   def show
     @competition = Competition.find(params[:id])
     @page_title = @competition.name
