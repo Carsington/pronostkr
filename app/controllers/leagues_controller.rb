f7937ba155ec61960e5539995f21f916b4106e4c
Alexandre FRASCHINI <alexandre.fraschini@gmail.com>
2020-02-28 17:34:51 +0100

    generated the forecasts controller & routes + started the JS on forecast team div

--- app/controllers/leagues_controller.rb
+++ app/controllers/leagues_controller.rb
@@ -1,30 +1,27 @@
 class LeaguesController < ApplicationController
 
-
   def show
     @league = League.find(params[:id])
   end
 
- def create
-  @league = League.new(league_params)
-  user_league = UserLeague.new(user: current_user, league: @league, is_owner: true)
+  def create
+    @league = League.new(league_params)
+    user_league = UserLeague.new(user: current_user, league: @league, is_owner: true)
 
-  if @league.save && user_league.save
-    redirect_to @league
-  else
-    render :new
+    if @league.save && user_league.save
+      redirect_to @league
+    else
+      render :new
+    end
   end
-end
 
-def new
-  @league = League.new
-end
+  def new
+    @league = League.new
+  end
 
-private
+  private
 
-def league_params
-  params.require(:league).permit(:name, :competition_id)
-end
-
-
+  def league_params
+    params.require(:league).permit(:name, :competition_id)
+  end
 end
