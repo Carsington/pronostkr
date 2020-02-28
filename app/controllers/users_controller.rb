d9b4d7d532e774fa4d443e37c8450d51d3a838b9
Pierre-Loïc Pichon <pierreloic.pichon@gmail.com>
2020-02-28 17:39:32 +0100

    render elements in dashboard

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,18 @@ class UsersController < ApplicationController
   def dashboard
     @page_title = "Tableau de bord"
     @user = current_user
+
+    if params[:search].present? && params[:search][:query].present?
+      @query = params[:search][:query]
+      @competitions = Competition.global_search(@query)
+    else
+      @competitions = Competition.all
+    end
+
+    if @competitions.empty?
+      @no_results = true
+      @competitions = Competition.all
+    end
   end
 
   def show
