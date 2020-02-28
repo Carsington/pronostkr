8e1184fc8c7412dbb2d1680afd18e76e30b0167c
Pierre-Loïc Pichon <pierreloic.pichon@gmail.com>
2020-02-28 15:09:55 +0100

    restore routes and add /hidden_components

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  root to: 'pages#home'
+  authenticated do
+    root to: redirect('/dashboard')
+  end
 
-  # authenticated do
-  #   root to: redirect('/dashboard')
-  # end
-
-  # unauthenticated do
-  #   root to: 'pages#home'
-  # end
+  unauthenticated do
+    root to: 'pages#home'
+  end
 
   get '/dashboard', to: 'users#dashboard', as: :dashboard_user
+  get '/hidden_components', to: 'pages#home', as: :hidden_components
 
   resources :users, only: [ :show, :edit ]
   resources :competitions, only: [ :show ]
