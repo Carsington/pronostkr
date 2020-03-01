cf5c488cab63b541ac15baa41d8edecca6dcc700
Pierre-Loïc Pichon <pierreloic.pichon@gmail.com>
2020-03-01 14:11:46 +0100

    fixed issue with upload Cloudinary

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,21 +6,9 @@ class User < ApplicationRecord
 
   has_one_attached :photo
 
-  after_create :attach_default_image
-
   has_many :user_leagues, dependent: :destroy
   has_many :leagues, through: :user_leagues
   has_many :forecasts, dependent: :destroy
 
   validates :username, presence: true, uniqueness: { case_sensitive: false }
-
-  private
-
-  def attach_default_image
-    self.photo.attach(
-      io: File.open(Rails.root.join("app", "assets", "images", "eddy_avatar.png")),
-      filename: 'eddy_avatar.png',
-      content_type: "image/png"
-    )
-  end
 end
