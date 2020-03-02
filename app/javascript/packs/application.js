12088c6170a71f3ab3e29d5e7dc94d2760640913
Pierre-Loïc Pichon <pierreloic.pichon@gmail.com>
2020-03-02 17:15:49 +0100

    wip forecast JS / AJAX

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,6 +1,6 @@
 import "bootstrap";
 import { placeForecast } from './place_forecast';
-import { initSortable } from '../../../src/plugins/init_sortable';
+// import { initSortable } from '../../../src/plugins/init_sortable';
 
-initSortable();
+// initSortable();
 placeForecast();
--- app/javascript/packs/place_forecast.js
+++ app/javascript/packs/place_forecast.js
@@ -1,4 +1,4 @@
-const toggleWinner = (teamOne, teamTwo) => {
+const toggleWinner = (teamOne, teamTwo, match) => {
   teamOne.addEventListener("click", (event) => {
     teamOne.classList.toggle("winning-team");
     teamTwo.classList.toggle("losing-team");
@@ -7,15 +7,31 @@ const toggleWinner = (teamOne, teamTwo) => {
       teamOne.classList.toggle("losing-team");
       teamTwo.classList.toggle("winning-team");
     }
+
+    updateForecast(match);
   });
+}
+
+const updateForecast = match => {
+  const form = match.querySelector('form');
+  const winnerForecast = match.querySelector('.winning-team');
+  const input = form.querySelector("#forecast_team");
+
+  if (winnerForecast) {
+    input.value = winnerForecast.dataset.teamId;
+  } else {
+    input.value = "";
+  }
+  
+  // form.submit();
 }
 
 const addTeamToggles = match => {
   const teamA = match.children["team-a"];
   const teamB = match.children["team-b"];
 
-  toggleWinner(teamA, teamB);
-  toggleWinner(teamB, teamA);
+  toggleWinner(teamA, teamB, match);
+  toggleWinner(teamB, teamA, match);
 }
 
 const placeForecast = () => {
