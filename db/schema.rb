4fb76114c3bb9a6f68092b969ff00c4037700d34
Pierre-Loïc Pichon <pierreloic.pichon@gmail.com>
2020-03-05 10:13:58 +0100

    fixed schema issue

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_04_114451) do
+ActiveRecord::Schema.define(version: 2020_03_04_134018) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -82,7 +82,6 @@ ActiveRecord::Schema.define(version: 2020_03_04_114451) do
     t.bigint "competition_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "name"
     t.index ["competition_id"], name: "index_matches_on_competition_id"
   end
 
